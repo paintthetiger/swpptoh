cd95c8c8d418a2806569a54722b8f3e1801f7567
Yongho <paintthetiger@gmail.com>
2017-10-25 19:36:00 +0900

    test 41%

--- hero/tests.py
+++ hero/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Hero
+from django.forms.models import model_to_dict
+import json
 
+class HeroTestCase(TestCase):
+    def setUp(self):
+        Hero.objects.create(name='Superman')
+        Hero.objects.create(name='Batman')
+        Hero.objects.create(name='Joker')
+
+        self.client = Client()
+
+    def test_hero_str(self):
+        batman = Hero.objects.get(name='Batman')
+        self.assertEqual(str(batman),'Batman')
+
+    def test_hero_detail_get(self):
+        response = self.client.get('/api/hero/1')
+
+        data = json.loads(response.content.decode())
+        self.assertEqual(data['name'], 'Superman')
+        self.assertEqual(response.status_code, 200)
+
+    def test_hero_list_get(self):
+        response = self.client.get('/api/hero/')
+
+        data = json.loads(response.content.decode())
+        self.assertEqual(len(data),3)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_hero_detail_get_fail(self):
+        response = self.client.get('/api/hero/4')
+
+        self.assertEqual(response.status_code, 404)
 # Create your tests here.
